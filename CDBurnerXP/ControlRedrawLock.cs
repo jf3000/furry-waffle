c362ea6f069ced242eb57cfa4982c2e117e6fa76
GeGeek <jf3000@gmail.com>
2019-11-04 16:07:31 +1000

    Add files via upload

---
+++ CDBurnerXP/ControlRedrawLock.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CDBurnerXP.Forms
+{
+    public class ControlRedrawLock : IDisposable
+    {
+        private Control _control;
+        private bool _invalidate;
+        private bool _disabledRedraw = false;
+
+        public ControlRedrawLock(Control control) : this(control, true)
+        {
+        }
+
+        public ControlRedrawLock(Control control, bool invalidate)
+        {
+            try
+            {
+                _control = control;
+                _invalidate = invalidate;
+
+                if (IsValidControl())
+                {
+                    // Lock drawing
+                    _control.SuspendLayout();
+                    _disabledRedraw = true;
+                }
+            }
+            catch
+            { }
+        }
+
+        private bool IsValidControl()
+        {
+            return ((_control != null) && (_control.IsHandleCreated));
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            try
+            {
+                if (IsValidControl() && _disabledRedraw)
+                {
+                    // Unlock drawing
+                    _control.ResumeLayout();
+                    if (_invalidate)
+                    {
+                        // Invalidate the control to trigger a re-paint.
+                        _control.Invalidate(true);
+                    }
+                }
+            }
+            catch
+            { }
+        }
+
+        #endregion
+    }
+}
---
+++ CDBurnerXP/SafeClipboard.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace CDBurnerXP.IO
+{
+    /// <summary>
+    /// Accesses the Clipboard without throwing exceptions.
+    /// </summary>
+    public static class SafeClipboard
+    {
+        public static bool IsDataPresent(string format)
+        {
+            try
+            {
+                IDataObject dataObject = Clipboard.GetDataObject();
+                if (dataObject != null)
+                    return dataObject.GetDataPresent(format);
+            }
+            catch { }
+            return false;
+        }
+
+        public static object GetData(string format)
+        {
+            try
+            {
+                IDataObject dataObject = Clipboard.GetDataObject();
+                if (dataObject != null)
+                    return dataObject.GetData(format);
+            }
+            catch { }
+            return null;
+        }
+
+        public static bool SetData(object value, bool copy)
+        {
+            try
+            {
+                Clipboard.SetDataObject(value, copy, 2, 50);
+                return true;
+            }
+            catch (ExternalException)
+            {
+                return false;
+            }
+        }
+    }
+}
